42a58efa92e5bdde48b36c6b6944b4dfdbf0336f
Mohamed <Mohamed@Houssainy>
2014-04-11 16:41:23 +0200

    Service and AsyncTask are ready to be used!

--- src/com/example/downloadmanager/DownloadService.java
+++ src/com/example/downloadmanager/DownloadService.java
@@ -1,26 +1,59 @@
 package com.example.downloadmanager;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
+import android.widget.Toast;
 
 public class DownloadService extends Service {
 
+	private Context context;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		context = this;
+	}
+
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		stopSelf();
+		new DownloadTask().execute(intent.getStringExtra("download-link"));
 		return super.onStartCommand(intent, flags, startId);
 	}
 
-	@Override
-	public IBinder onBind(Intent arg0) {
-		return null;
+	private class DownloadTask extends AsyncTask<String, Void, Void> {
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			Toast.makeText(context, "Starting Downloading...",
+					Toast.LENGTH_SHORT).show();
+		}
+
+		@Override
+		protected Void doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			System.out.println("Back ground");
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			Toast.makeText(context, "Downloading Finished...",
+					Toast.LENGTH_SHORT).show();
+		}
 	}
 
+	/**
+	 * Must Override this method and return null to avoid Bind Service
+	 */
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
 }
