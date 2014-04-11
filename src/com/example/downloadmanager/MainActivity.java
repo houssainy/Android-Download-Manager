7292c3db56b420e2e1c33596122a567a577ca557
Mohamed <Mohamed@Houssainy>
2014-04-11 16:48:20 +0200

    UI Done

--- src/com/example/downloadmanager/MainActivity.java
+++ src/com/example/downloadmanager/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -15,15 +16,16 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		download = (Button) findViewById(R.id.download);
+		download = (Button) findViewById(R.id.download_button);
 		download.setOnClickListener(this);
-
 	}
 
 	@Override
 	public void onClick(View v) {
 		Intent intent = new Intent(this, DownloadService.class);
-		intent.putExtra("download-link", "My Link");
+		intent.putExtra("download-link",
+				((EditText) findViewById(R.id.download_url)).getText()
+						.toString());
 		startService(intent);
 	}
 
